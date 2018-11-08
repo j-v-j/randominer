a97e560ad46b94e129f473973553aa5ec867cb65
Jyri Järvinen <jyri@jarvinen.io>
2018-11-08 15:04:27 +0200

    Add tests for getting random Twitch stream

--- TestRandominer/Twitch/TestTwitchApi.cs
+++ TestRandominer/Twitch/TestTwitchApi.cs
@@ -5,19 +5,45 @@ using Xunit;
 using Moq;
 using Randominer.Twitch;
 using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using Xunit.Abstractions;
 
 namespace TestRandominer.Twitch
 {
-    class TestTwitchApi
+    public class TestTwitchApi
     {
         private Mock<FakeHttpMessageHandler> _fakeHttpMessageHandler;
         private TwitchClient _twitchClient;
+        private ITestOutputHelper _outputter;
 
-        public TestTwitchApi()
+        public TestTwitchApi(ITestOutputHelper output)
         {
+            _outputter = output;
             _fakeHttpMessageHandler = new Mock<FakeHttpMessageHandler> { CallBase = true };
             _twitchClient = new TwitchClient("1234",new HttpClient(_fakeHttpMessageHandler.Object));
         }
+        
+        [Fact]
+        public async System.Threading.Tasks.Task TestGetRandomStreamMustHaveOneStream()
+        {
+            _fakeHttpMessageHandler.Setup(f => f.Send(It.IsAny<HttpRequestMessage>())).Returns(new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent
+                (File.ReadAllText("streams.json"))
+            });
 
+            var api = new TwitchApi("1234", _twitchClient);
+
+            var result = await api.GetRandomStream();
+            
+            _outputter.WriteLine($"Stream title: {result.title}" );
+            _outputter.WriteLine($"Stream username: {result.user_name}");
+
+            Assert.IsType<StreamDTO>(result);
+
+        }
     }
 }
