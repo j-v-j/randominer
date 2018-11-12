eee0b167acffe0fb46f230a3c9c3eeb9a281cfa9
Jyri Järvinen <jyri@jarvinen.io>
2018-11-12 12:02:14 +0200

    Add twitch random stream client side and controller

---
+++ Randominer/ClientApp/src/components/Twitch.js
@@ -0,0 +1,50 @@
+﻿import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const TWITCH_EMBED_URL = "https://embed.twitch.tv/embed/v1.js";
+const TWITCH_URL = "https://twitch.tv/"
+export class Twitch extends Component {
+
+    componentDidMount() {
+        const script = document.createElement('script');
+        script.setAttribute(
+            'src',
+            TWITCH_EMBED_URL
+        );
+
+        document.body.appendChild(script);
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { streamData: '', error: '' }
+
+        fetch('api/twitch/RandomStream')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ streamData: data });
+                console.log(data.user_name);
+                new window.Twitch.Embed('twitch-embed', {
+                    width: 854,
+                    height: 480,
+                    channel: data.user_name,
+                    layout: "video"
+                });
+            })
+            .catch(response => this.setState({ error: response }));
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> Random Twitch Stream</h1>
+                <a href={ TWITCH_URL + this.state.streamData.user_name}>{this.state.streamData.title}</a>
+                <div id="twitch-embed"></div>
+            </div>
+        );
+        
+    }
+}
+export default Twitch;
---
+++ Randominer/Controllers/TwitchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Randominer.Settings;
+using Randominer.Twitch;
+
+namespace Randominer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TwitchController : ControllerBase
+    {
+
+        private ApiKeys _apikeys;
+
+        public TwitchController(IOptions<ApiKeys> apikeys)
+        {
+            _apikeys = apikeys.Value;
+
+        }
+
+        [HttpGet("[action]")]
+        public async Task<StreamDTO> RandomStream()
+        {
+            var twitchApi = new TwitchApi(_apikeys.twitch);
+            return await twitchApi.GetRandomStream();
+        }
+
+    }
+}
