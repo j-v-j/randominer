ae6db99ba40d6a82fcd73170a1de52ee938f15cb
Jyri Järvinen <jyri@jarvinen.io>
2018-11-19 00:15:49 +0200

    Add a reroll button to random twitch stream

--- Randominer/ClientApp/src/components/Twitch.js
+++ Randominer/ClientApp/src/components/Twitch.js
@@ -5,8 +5,8 @@ import { Link } from 'react-router-dom';
 
 const TWITCH_EMBED_URL = "https://embed.twitch.tv/embed/v1.js";
 const TWITCH_URL = "https://twitch.tv/"
-export class Twitch extends Component {
 
+export class Twitch extends Component {
     componentDidMount() {
         const script = document.createElement('script');
         script.setAttribute(
@@ -17,23 +17,39 @@ export class Twitch extends Component {
         document.body.appendChild(script);
     }
 
-    constructor(props) {
-        super(props);
-        this.state = { streamData: '', error: '' }
+    handleClick() {
+        this.fetchVideo();
+    }
 
+    fetchVideo() {
         fetch('api/twitch/RandomStream')
             .then(response => response.json())
             .then(data => {
                 this.setState({ streamData: data });
                 console.log(data.user_name);
-                new window.Twitch.Embed('twitch-embed', {
-                    width: 854,
-                    height: 480,
-                    channel: data.user_name,
-                    layout: "video"
-                });
+                if (!this.embedPlayer) {
+                    this.embedPlayer = new window.Twitch.Embed('twitch-embed', {
+                        width: 854,
+                        height: 480,
+                        channel: data.user_name,
+                        layout: "video"
+                    });
+                }
+                else {
+                    let player = this.embedPlayer.getPlayer();
+                    player.setChannel(data.user_name);
+                }                
             })
             .catch(response => this.setState({ error: response }));
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = { streamData: '', error: '' }
+
+        this.handleClick = this.handleClick.bind(this);
+
+        this.fetchVideo();
     }
 
     render() {
@@ -42,6 +58,7 @@ export class Twitch extends Component {
                 <h1> Random Twitch Stream</h1>
                 <a href={ TWITCH_URL + this.state.streamData.user_name}>{this.state.streamData.title}</a>
                 <div id="twitch-embed"></div>
+                <button onClick={this.handleClick}>Reroll</button>
             </div>
         );
         
